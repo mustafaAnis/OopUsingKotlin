47f4f60456cdf4d98b40fb965555360e6705f25f
mustafa <mustafaAnis>
2022-08-08 21:29:28 +0500

    end more lectures in day 2

---
+++ app/src/main/java/com/example/oopusingkotlin/Dataclass.kt
@@ -0,0 +1,21 @@
+package com.example.oopusingkotlin
+
+data class User(val id:Long, var name: String )
+
+fun main(){
+    val user1 = User(1,"mustafa")
+
+//    val name = user1.name
+    user1.name = "Osama"
+//    println(name)
+    val user2 = User(1,"Osama")
+    println(user1 == user2)
+    val updatedUser = user1.copy(name = "safeer")
+    val (id,name) = User(32,"Hamza")
+    println("$name has id is $id" )
+    println(user1)
+    println(user2)
+    println(updatedUser)
+    println(updatedUser.component1())
+    println(updatedUser.component2())
+}
--- app/src/main/java/com/example/oopusingkotlin/RemainingOop.kt
+++ app/src/main/java/com/example/oopusingkotlin/RemainingOop.kt
@@ -1,2 +1,58 @@
 package com.example.oopusingkotlin
 
+import android.graphics.ColorSpace
+
+fun main(){
+    var mustafa = Person("Mustafa","Anis",32)
+
+    mustafa.age = 35
+    println("Mustafa is ${mustafa.age} years old")
+    mustafa.stateHobby()
+
+
+
+
+
+//    mustafa.hobby = "Cricket"
+//    var john = Person()
+//    john.hobby = "football"
+//    john.stateHobby()
+//    var johncorner = Person(lastName = "petterson")
+
+}
+class Person(firstName: String = "john", lastName: String = "Doe"){
+    var age : Int? = null
+    var hobby : String = "Watch Netflix"
+    var firstName: String? = null
+    init {
+        this.firstName = firstName
+     println("person created")
+     println("firstname = $firstName lastname = $lastName")
+ }
+    constructor(firstName: String,lastName: String,age: Int)
+            : this(firstName,lastName){
+        this.age = age
+        println("firstname = $firstName lastname = $lastName , age = $age")
+
+    }
+
+    fun stateHobby(){
+        println("$firstName\'s hobby is $hobby")
+    }
+
+
+}
+//
+//fun main(){
+//    var samsung = MobilePhone("android","samsung","A21s")
+//    var iphone  = MobilePhone("ios","iphone","7plus")
+//    var huawei = MobilePhone ("huawei_software","huawei","nova7i")
+//
+//}
+//class MobilePhone(osName: String,brand: String,model: String){
+//
+//    init {
+//        println("OsName = $osName brand = $brand model = $model")
+//    }
+//
+//}
